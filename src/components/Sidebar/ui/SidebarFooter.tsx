1ee56719f0214ea3e8d7cc968e34180d5b15c2a4
Vladislav <kalachinskii_vl@mail.ru>
2025-05-28 18:11:32 +0300

    test: проверка пустого изменения - prettier

--- src/components/Sidebar/ui/SidebarFooter.tsx
+++ src/components/Sidebar/ui/SidebarFooter.tsx
@@ -1,10 +1,13 @@
 export function SidebarFooter() {
   return (
+    // тест
+
     <div className="sidebar-footer">
       <div className="theme-toggle">
         <img src="assets/icon-light-theme.svg" alt="" />
         <label className="switch">
           <input type="checkbox" />
+
           <span className="slider round"></span>
         </label>
         <img src="assets/icon-dark-theme.svg" alt="" />
