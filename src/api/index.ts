dc4353fb52b0d4a41f681d7ccf73161d9800e9e7
amogus22877769 <y.lev357@gmail.com>
2025-07-17 23:38:52 +0300

    init

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosError } from "axios";
-import { ZodError, ZodType } from "zod";
+import z, { ZodError, ZodType } from "zod";
 
 export async function getData<T>(
   path: string,
@@ -48,6 +48,109 @@ export async function updata<D, T>(
     if (error instanceof AxiosError || error instanceof ZodError) {
       console.error("Request failed:", error);
       alert("Ошибка на сервере, не получилось сохранить изменеия");
+    } else {
+      console.error("Unexpected error:", error);
+      alert("Какая-то ошибка, не получилось сохранить изменеия");
+    }
+  }
+}
+
+export async function isUsernameFree(
+  username: string,
+): Promise<boolean | undefined> {
+  try {
+    const response = await axios.post<unknown>(
+      "/api/check_username",
+      { username: username },
+      {
+        headers: {
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+      },
+    );
+    if (response.status == 200) {
+      return false;
+    }
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.status === 400) {
+        return true;
+      } else {
+        console.error("Request failed:", error);
+        alert("Ошибка на сервере, не получилось сохранить изменеия");
+      }
+    } else {
+      console.error("Unexpected error:", error);
+      alert("Какая-то ошибка, не получилось сохранить изменеия");
+    }
+  }
+}
+
+export async function signUp(
+  username: string,
+  password: string,
+): Promise<string | undefined> {
+  try {
+    const response = await axios.post<unknown>(
+      "/api/signup",
+      { username: username, password: password },
+      {
+        headers: {
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+      },
+    );
+
+    return z
+      .object({
+        token: z.string(),
+      })
+      .parse(response.data).token;
+  } catch (error) {
+    if (error instanceof AxiosError || error instanceof ZodError) {
+      console.error("Request failed:", error);
+      alert("Ошибка на сервере, не получилось сохранить изменеия");
+    } else {
+      console.error("Unexpected error:", error);
+      alert("Какая-то ошибка, не получилось сохранить изменеия");
+    }
+  }
+}
+
+export async function logIn(
+  username: string,
+  password: string,
+): Promise<string | undefined> {
+  try {
+    const response = await axios.post<unknown>(
+      "/api/signin",
+      { username: username, password: password },
+      {
+        headers: {
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+      },
+    );
+
+    return z
+      .object({
+        token: z.string(),
+      })
+      .parse(response.data).token;
+  } catch (error) {
+    if (error instanceof AxiosError || error instanceof ZodError) {
+      if (
+        error instanceof AxiosError &&
+        (error.status === 400 || error.status === 401)
+      ) {
+        console.log("SignUp failed");
+      } else {
+        console.error("Request failed:", error);
+        alert("Ошибка на сервере, не получилось сохранить изменеия");
+      }
     } else {
       console.error("Unexpected error:", error);
       alert("Какая-то ошибка, не получилось сохранить изменеия");
